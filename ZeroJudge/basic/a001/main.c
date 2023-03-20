a2c32c29aee68807458f15a5bd372114736044e2
thomascwei <thomas.c.wei@gmail.com>
2023-03-20 20:25:26 +0800

    .

---
+++ ZeroJudge/basic/a001/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  // 宣告一個字串
+  char jim[20];
+  // 從使用者輸入存到儲存空間
+  scanf("%s", jim);
+  // 印出答案
+  printf("hello, %s\n",jim); 
+  return 0;
+}
---
+++ ZeroJudge/basic/a002/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  // 宣告兩個整數
+  int a,b;
+  // 從使用者輸入存到儲存空間
+  scanf("%d %d",&a,&b);
+  // 輸出
+  printf("%d\n",a+b);
+  return 0;
+}
